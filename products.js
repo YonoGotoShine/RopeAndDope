e5ab5f4ab85f67effa18fde279cd1459d34c6818
YonoGotoShine <deadxlej@gmail.com>
2021-05-28 18:36:15 +0200

    Ez

--- products.js
+++ products.js
@@ -2,5 +2,18 @@
 
 class Product
 {
-    var name
+    constructor(name, description, price, img, id_category)
+    {
+        this.name = name;
+        this.description = description, 
+        this.price = price;
+        this.img = img;
+        this.id_category = id_category; 
+        
+    }
+
 }
+
+var p1 = new Product("Zioło", "Zioło o smaku lemon haze", 50, "icons/icon_weed.png", 1);
+
+document.write("Nazwa: " + p1.name + " Opis: " + p1.description + " Cena: " + p1.prince +" obrazek: " + "<img src=" + p1.img + ">" + " Kategoria: " + p1.id_category)
