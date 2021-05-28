ce0ed72693889fd34ebcc0932c19cc1d642b36f0
YonoGotoShine <deadxlej@gmail.com>
2021-05-28 19:55:53 +0200

    Update products.js

--- products.js
+++ products.js
@@ -15,18 +15,15 @@ class Product
         this.id_category = id_category;  
     }
 
-    pokaz()
-    {
-        for(let i=0; i<Product_table.length; i++)
-        {
-            document.write("Nazwa: " + Product_table[i].name + " Opis: " +Product_table[i].description + " Cena: " + Product_table[i].price +" obrazek: " + "<img src=" + Product_table[i].img + ">" + " Kategoria: " + Product_table[i].id_category + "<br>")
-        }
-    }
-
-
 }
 
-
+function pokaz()
+{
+    for(let i=0; i<Product_table.length; i++)
+    {
+        document.write("Nazwa: " + Product_table[i].name + " Opis: " +Product_table[i].description + " Cena: " + Product_table[i].price +" obrazek: " + "<img src=" + Product_table[i].img + ">" + " Kategoria: " + Product_table[i].id_category + "<br>")
+    }
+}
 
 
 var p1 = new Product("Zioło", "Zioło o smaku lemon haze", 50 , "icons/cytrynowa_mgieka.jpg", 1);
@@ -35,4 +32,4 @@ var p2 = new Product("kwas", "niezłe kwasidło", 70, "icons/icon_weed.png", 1);
 
 var Product_table =[p1, p2];
 
-p1.pokaz();
+pokaz();
