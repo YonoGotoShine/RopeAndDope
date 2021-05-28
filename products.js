d519abd6e113e0d43a6f0ee98be2ca6d270704cf
YonoGotoShine <deadxlej@gmail.com>
2021-05-28 19:42:22 +0200

    Kolejny kod

--- products.js
+++ products.js
@@ -1,5 +1,9 @@
 
+//tablica obiektów
 
+var Product_table =[p1, p2];
+
+//clasa Product opisująca właściwości produktu
 class Product
 {
     constructor(name, description, price, img, id_category)
@@ -8,12 +12,27 @@ class Product
         this.description = description, 
         this.price = price;
         this.img = img;
-        this.id_category = id_category; 
-        
+        this.id_category = id_category;  
     }
+
+    pokaz()
+    {
+        for(let i=0; i<Product_table.length; i++)
+        {
+            document.write("Nazwa: " + Product_table[i].name + " Opis: " +Product_table[i].description + " Cena: " + Product_table[i].price +" obrazek: " + "<img src=" + Product_table[i].img + ">" + " Kategoria: " + Product_table[i].id_category + "<br>")
+        }
+    }
+
 
 }
 
-var p1 = new Product("Zioło", "Zioło o smaku lemon haze", 50, "icons/icon_weed.png", 1);
 
-document.write("Nazwa: " + p1.name + " Opis: " + p1.description + " Cena: " + p1.prince +" obrazek: " + "<img src=" + p1.img + ">" + " Kategoria: " + p1.id_category)
+
+
+var p1 = new Product("Zioło", "Zioło o smaku lemon haze", 50 , "icons/cytrynowa_mgieka.jpg", 1);
+
+var p2 = new Product("kwas", "niezłe kwasidło", 70, "icons/icon_weed.png", 1);
+
+var Product_table =[p1, p2];
+
+p1.pokaz();
