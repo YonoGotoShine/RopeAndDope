b5955af80a7330c2851c71ec766774abb3ba758e
YonoGotoShine <deadxlej@gmail.com>
2021-06-11 12:41:34 +0200

    43234

--- products.js
+++ products.js
@@ -1,18 +1,23 @@
+//tablica obiektów
+var Product_table =[];
+
+var l = 0;
 
 //clasa Product opisująca właściwości produktu
 class Product
 {
+
     constructor(name, description, price, img, id_category)
     {
         this.name = name;
         this.description = description, 
         this.price = price;
         this.img = img;
-        this.id_category = id_category;  
+        this.id_category = id_category; 
+        this.id = l; 
     }
 
 }
-
 
 
 //funkcja pokazująca tablice
@@ -20,21 +25,42 @@ function pokaz()
 {
     for(let i=0; i<Product_table.length; i++)
     {
-        document.write("Nazwa: " + Product_table[i].name + " Opis: " +Product_table[i].description + " Cena: " + Product_table[i].price +" obrazek: " + "<img src=" + Product_table[i].img + ">" + " Kategoria: " + Product_table[i].id_category + "<br>")
+        document.write("Nazwa: " + Product_table[i].name + " Opis: " + Product_table[i].description + " Cena: " + Product_table[i].price +" obrazek: " + "<img src=" + Product_table[i].img + ">" + " Kategoria: " + Product_table[i].id_category + " Id: " + Product_table[i].id +"<br>")
     }
+}
+
+//Dodaj Produkt
+function dodaj_produkt(name, description, price, img, id_category) 
+{ 
+    l++;
+    let p = new Product(name, description, price , img, id_category);
+
+    Product_table[Product_table.length] = p;
+
+    localStorage.setItem("tablica_p", Product_table)
+    
 }
 
 
 
-var p1 = new Product("Zioło", "Zioło o smaku lemon haze", 50 , "icons/cytrynowa_mgieka.jpg", 1);
+dodaj_produkt("Zioło", "Zioło o smaku lemon haze", 50 , "icons/cytrynowa_mgieka.jpg", 1);
 
-var p2 = new Product("kwas", "niezłe kwasidło", 70, "icons/icon_weed.png", 1);
+dodaj_produkt("kwas", "niezłe kwasidło", 70, "icons/icon_weed.png", 2);
+
+dodaj_produkt("kwa5423543s", "niezłe kwasidło", 70, "icons/icon_weed.png", 1);
+dodaj_produkt("kwa5423543s", "niezłe kwasidło", 70, "icons/icon_weed.png", 1);
 
 
-//tablica obiektów
-var Product_table =[p1, p2];
 
-pokaz()
+
+document.write(localStorage.getItem("tablica_p"))
+
+pokaz();
+
+
+
+
+
   
 
 
