1b21794afc11e48e899d061ab577fa739c1bb45c
YonoGotoShine <deadxlej@gmail.com>
2021-06-12 20:49:11 +0200

    3

--- products.js
+++ products.js
@@ -29,7 +29,7 @@ function pokaz(categoria, sort)
     {
         if(categoria == Product_table[i].id_category)
         {
-            document.write('<div class="produkt"><div id="zdjecie"><img src=' + Product_table[i].img + '></div><div id="obok"> '+'<p>Nazwa: ' + Product_table[i].name +' Opis: ' + Product_table[i].description +'<br>'+' Cena: '+ Product_table[i].price +'zł'+  '</div><br><br><br></div>')
+            document.write('<div class="produkt"><div id="zdjecie"><img src=' + Product_table[i].img + '></div><div id="obok"> '+'<p>Nazwa: ' + Product_table[i].name +' Opis: ' + Product_table[i].description +'<br>'+' Cena: '+ Product_table[i].price +'zł'+  '</div><button>Dodaj do koszyka</button><br><br><br></div>')
         }
     }
 }
